5dc7cf93dc56db1baf92108f377f8aab5be2f921
Yannick Hold <yannickhold@gmail.com>
2014-10-05 14:42:27 -0400

    * Improved parsing script

--- scripts/parse.py
+++ scripts/parse.py
@@ -7,24 +7,32 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
-with open(sys.argv[1], 'rb') as fhdl:
-    raw_data = fhdl.readlines()
 
-data = defaultdict(list)
+def parseFile(filename):
+    with open(filename, 'rb') as fhdl:
+        raw_data = fhdl.readlines()
 
-for line in raw_data:
-    key, value = line.decode('utf-8', 'replace').split(":", maxsplit=1)
-    key = str(key.strip())
-    if key not in ("Accel", "Gyro", "Magneto"):
-        print("Could not process line: ", line)
-        continue
-    data[key].append([float(re.sub("[^0-9-.]", "", x.strip())) for x in value.split(',')])
+    data = defaultdict(list)
 
-print(data)
+    for line in raw_data:
+        key, value = line.decode('utf-8', 'replace').split(":", maxsplit=1)
+        key = str(key.strip())
+        if key not in ("Accel", "Gyro", "Magneto"):
+            print("Could not process line: ", line)
+            continue
+        data[key].append([float(re.sub("[^0-9-.]", "", x.strip())) for x in value.split(',')])
+    return data
 
-for key, value in data.items():
-    for axis in zip(*value):
-        print(axis)
-        plt.plot(axis)
-    plt.savefig('{}.png'.format(key))
 
+def plotData(data):
+    for key, value in data.items():
+        print(key)
+        for axis in zip(*value):
+            print(axis)
+            plt.plot(axis)
+        plt.savefig('{}.png'.format(key))
+
+
+if __name__ == '__main__':
+    data = parseFile(sys.argv[1])
+    plotData(data)
